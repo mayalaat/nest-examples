0ac67cac2dc896e4c23f0d7072eb8855bebcec64
Alejandro Ayala <mayalaat@pichincha.com>
2022-06-29 12:03:43 -0500

    Change Error to NotFoundException

--- car-api/src/users/users.service.ts
+++ car-api/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -32,7 +32,7 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>): Promise<User> {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     Object.assign(user, attrs);
 
@@ -42,7 +42,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
 
     return this.repository.remove(user);
