e5ff68f83b2ba5ae84c9aa68aa9326fea64811aa
Alejandro Ayala <mayalaat@pichincha.com>
2022-06-28 20:59:37 -0500

    Implementing user create service

--- car-api/src/users/users.controller.ts
+++ car-api/src/users/users.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('signup')
-  createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+  createUser(@Body() body: CreateUserDto): Promise<User> {
+    return this.usersService.create(body.email, body.password);
   }
 }
--- car-api/src/users/users.service.ts
+++ car-api/src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  create(email: string, password: string): Promise<User> {
+    const user = this.repository.create({ email, password });
+
+    return this.repository.save(user);
+  }
+}
