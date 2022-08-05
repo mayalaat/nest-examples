dcbbc6a8c0d27a2a03ab0dfe27104ee261b9eeb5
Alejandro Ayala <mayalaat@pichincha.com>
2022-08-05 17:11:22 -0500

    Adding signs up test

--- car-api/src/users/auth.service.spec.ts
+++ car-api/src/users/auth.service.spec.ts
@@ -2,13 +2,15 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // create a fake copy of the Users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -29,5 +31,38 @@ describe('AuthService', () => {
 
   it('Can create an instance of auth service', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('Creates a new user with a salted and hashed password', async () => {
+    const user = await service.signUp('email@mail.com', 'pass');
+
+    expect(user.password).not.toEqual('pass');
+
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throw an exception if user signs up with email that is in use. Try-catch way', async () => {
+    const fakeUser = { id: 1, email: '', password: '' } as User;
+    fakeUsersService.find = () => Promise.resolve([fakeUser]);
+
+    try {
+      await service.signUp('email@mail.com', 'pass');
+    } catch (err) {
+      expect(err).toBeDefined();
+      expect(err).toBeInstanceOf(BadRequestException);
+      expect(err.message).toBe('email in use');
+    }
+  });
+
+  it('throw an exception if user signs up with email that is in use. Rejects way', async () => {
+    const fakeUser = { id: 1, email: '', password: '' } as User;
+    fakeUsersService.find = () => Promise.resolve([fakeUser]);
+
+    expect.assertions(1);
+    await expect(
+      service.signUp('email@mail.com', 'pass'),
+    ).rejects.toBeInstanceOf(BadRequestException);
   });
 });
