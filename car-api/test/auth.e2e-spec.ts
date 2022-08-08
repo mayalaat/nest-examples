73d4a04214f15392597a36207bca889cddceb0b9
Alejandro Ayala <mayalaat@pichincha.com>
2022-08-08 17:57:42 -0500

    Adding authorization E2E test

---
+++ car-api/test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Authentication system (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('Handles a signUp request', () => {
+    const myEmail = 'emai@mail.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: myEmail, password: 'pass' })
+      .expect(201)
+      .then((response) => {
+        const { id, email } = response.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(myEmail);
+      });
+  });
+});
